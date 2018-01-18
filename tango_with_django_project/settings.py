946f2cb89fe115da72f3f520a6057908251b6d35
Jonathan Sprague <2244726s@student.gla.ac.uk>
2018-01-18 17:53:11 +0000

    reached 3.6 using python anywhere

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9%ch0hl4orl*foufqf7r3f)78=2w-fay@hl42r^mn3i^)%$9bz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
