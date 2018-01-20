b1aa74118275f9307c74a43d58cb13a172bb5a1f
Jonathan Sprague <2244726s@student.gla.ac.uk>
2018-01-20 10:53:24 +0000

    reached 4.3 using python anywhere, Rango image still doesn't load

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -25,9 +25,9 @@ SECRET_KEY = '9%ch0hl4orl*foufqf7r3f)78=2w-fay@hl42r^mn3i^)%$9bz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0"]
+ALLOWED_HOSTS = ["127.0.0.1","0.0.0.0",'2244726s.pythonanywhere.com']
 
-
+print(STATIC_DIR)
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rango',
 ]
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
